21bf50caae2b81f0e3e6060b65339162784b924e
Sean Moran <spmoran@hotmail.com>
2022-10-07 11:08:39 -0400

    aaa

--- Api/Repository.cs
+++ Api/Repository.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
 using Newtonsoft.Json;
 
 namespace Api
