3537cb31b46348d83deafe7151afa5d4b689ecdd
Sean Moran <spmoran@hotmail.com>
2022-10-09 11:53:50 -0400

    aaa

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,18 +2,25 @@ using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Runtime.CompilerServices;
+using AzureStaticWebApps.Blazor.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddMsalAuthentication(options =>
-
-{
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-});
-builder.Services.AddMudServices();
+builder.Services
+    .AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddStaticWebAppsAuthentication()
+    .AddMudServices();
 
 await builder.Build().RunAsync();
+
+
+//builder.Services.AddStaticWebAppsAuthentication(this IServiceCollection);
+//builder.Services.AddMsalAuthentication(options =>
+
+//{
+//    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+//});
