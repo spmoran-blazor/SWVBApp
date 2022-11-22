0f5ee07fe77deb9aa8c3a517ab51bf37e4f80bdd
Sean Moran <spmoran@hotmail.com>
2022-11-22 10:31:37 -0500

    Push for testing

--- Shared/Services/StateAccess.cs
+++ Shared/Services/StateAccess.cs
@@ -3,10 +3,6 @@ namespace WVBApp.Shared.Services.State
 {
     public class StateAccessService
     {
-        public StateAccessService()
-        {
-
-        }
         public event Action? OnStateChange;
 
         public Member? Member { get; set; }
@@ -20,34 +16,19 @@ namespace WVBApp.Shared.Services.State
         {
             UserId = value;
             NotifyStateChanged();
-            //OnStateChange?.Invoke();
         }
         public void SetMemberValue(Member? value)
         {
             Member = value;
             NotifyStateChanged();
-            //OnStateChange?.Invoke();
         }
 
         public void SetUserJsonValue(string value)
         {
             UserJson = value;
             NotifyStateChanged();
-            //OnStateChange?.Invoke();
         }
 
         private void NotifyStateChanged() => OnStateChange?.Invoke();
-    }
-
-    public interface IStateAccessService
-    {
-        event Action? OnStateChange;
-        string? Value { get; set; }
-        string? UserJson { get; set; }
-        string? UserEmail { get; set; }
-        string? UserName { get; set; }
-        string? UserId { get; set; }
-        void SetUserIdValue(string value);
-        void SetUserJsonValue(string value);
     }
 }
